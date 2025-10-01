2b6df9abbaf0037d5ae757e61613ce1ff4c8b540
ednaldo.junior <ednaldo.junior@prill.com.br>
2025-10-01 16:54:36 -0300

    feat: implementando funcionalidades de reviews para que os teste de integração passem

---
+++ src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -0,0 +1,35 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.ReviewRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+    private final AuthService authService;
+
+    public ReviewService(ReviewRepository reviewRepository, AuthService authService) {
+        this.reviewRepository = reviewRepository;
+        this.authService = authService;
+    }
+
+    @Transactional
+    public ReviewDTO insert(ReviewDTO dto) {
+        Review review = new Review();
+        review.setText(dto.getText());
+        Movie movie = new Movie();
+        movie.setId(dto.getMovieId());
+        review.setMovie(movie);
+        User user = authService.authenticated();
+        review.setUser(user);
+        review = reviewRepository.save(review);
+        return new ReviewDTO(review);
+
+    }
+}
