09310965eb30e2c7018b5d4ec68bb163e411e843
ednaldo.junior <ednaldo.junior@prill.com.br>
2025-09-30 22:56:11 -0300

    feat: implementando funcionalidades de genres para que os teste de integração passem

--- src/main/java/com/devsuperior/movieflix/controllers/GenreController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/GenreController.java
@@ -1,8 +1,14 @@
 package com.devsuperior.movieflix.controllers;
 
+import com.devsuperior.movieflix.dto.GenreDTO;
 import com.devsuperior.movieflix.services.GenreService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/genres")
@@ -12,5 +18,11 @@ public class GenreController {
 
     public GenreController(GenreService genreService) {
         this.genreService = genreService;
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_VISITOR', 'ROLE_MEMBER')")
+    @GetMapping
+    public ResponseEntity<List<GenreDTO>> getGenres() {
+        return ResponseEntity.ok(genreService.getGenres());
     }
 }
--- src/main/java/com/devsuperior/movieflix/services/GenreService.java
+++ src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -1,7 +1,23 @@
 package com.devsuperior.movieflix.services;
 
+import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.repositories.GenreRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GenreService {
+
+    private final GenreRepository genreRepository;
+
+    public GenreService(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
+
+    public List<GenreDTO> getGenres() {
+        List<Genre> list = genreRepository.findAll();
+        return list.stream().map(GenreDTO::new).toList();
+    }
 }
